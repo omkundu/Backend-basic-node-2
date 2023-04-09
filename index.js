6c55f953275499d656a97a5dd4826c78e62af071
omkundu <omkundu156@gmail.com>
2023-04-09 19:49:55 +0530

    writeFile-commit-upadted

--- index.js
+++ index.js
@@ -6,16 +6,34 @@
 
 const fs=require("fs");
 
-console.log(x.appendFile());
-fs.readFile("./lecture.txt",{encoding:"utf-8"},(err,data)=>{
+// console.log(x.appendFile());
+// fs.readFile("./lecture.txt",{encoding:"utf-8"},(err,data)=>{
+//     if(err){
+//         console.log("Error Reading the file")
+//         console.log(err);
+//     }
+//     console.log(data);
+// })
+
+// const data=fs.readFileSync("./lecture.txt",{encoding:"utf-8"})
+// console.log(data)
+
+// console.log("bye")
+
+
+// fs.writeFile("./test.text","Second hello from index.js",{encoding:"utf-8"},(err)=>{
+//     if(err){
+//         console.log("Err writing to the file")
+//         console.log(err)
+//     }
+//     console.log("Go Check the file now")
+// })
+
+
+fs.appendFile("./test.text","\n  fourth hello from index.js \n",{encoding:"utf-8"},(err)=>{
     if(err){
-        console.log("Error Reading the file")
-        console.log(err);
+          console.log(err)
     }
-    console.log(data);
+        console.log("Go Check the file now")
+
 })
-
-const data=fs.readFileSync("./lecture.txt",{encoding:"utf-8"})
-console.log(data)
-
-console.log("bye")
